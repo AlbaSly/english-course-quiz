f035e57e2d0eb70d251ebf69d137523503b0698f
AlbaSly <arias.brando.raxel@gmail.com>
2022-04-01 23:01:17 -0600

    Fix questions

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ function App() {
 
   return (
     <div className='App'>
-      <div className='bg'>
-      </div>
       <Quiz/>
     </div>
   )
--- src/global/questions.ts
+++ src/global/questions.ts
@@ -2,54 +2,88 @@ import Question from "../interfaces/questions.interface";
 
 const QUESTIONS: Question[] = [
     {
-        question: "Language ability is typically measured in what parts",
+        question: "What did parents think about learning a second language in children?",
         answers: [
-            {answerText: 'Conjugation and expanse of vocabulary', isCorrect: false},
-            {answerText: 'Talking, writing, listening and reading', isCorrect: true},
-            {answerText: 'Pronunciation and sentence structure', isCorrect: false},
-            {answerText: 'Accent, speed and spelling', isCorrect: false}
+            {answerText: 'It\'s bad for kid\'s cognitive development', isCorrect: false},
+            {answerText: 'Bad grades', isCorrect: false},
+            {answerText: 'Lower IQ', isCorrect: false},
+            {answerText: 'All of the above', isCorrect: true}
         ]
     },
     {
-        question: "A person who learns French and English simultaneously from childhood onward would be considered which type of bilingual?",
+        question: "Where appeared the next line \"Being bilingual makes you smarter and more creative\"",
         answers: [
-            {answerText: 'Subordinate bilingual', isCorrect: false},
-            {answerText: 'Coordinate bilingual', isCorrect: false},
-            {answerText: 'Compound bilingual', isCorrect: true},
-            {answerText: 'All of the above', isCorrect: false}
+            {answerText: 'In the headlines', isCorrect: false},
+            {answerText: 'From scientists', isCorrect: false},
+            {answerText: 'In researches', isCorrect: false},
+            {answerText: 'In the headlines and researches', isCorrect: true}
         ]
     },
     {
-        question: "Which part of the brain is is responsible for logical thinking?",
+        question: "\"teach your child French and you get a better child! more creativity, multitasking, and academic performance in other subjects\", it's true?",
         answers: [
-            {answerText: 'Cerebellum', isCorrect: false},
-            {answerText: 'Right Hemisphere', isCorrect: false},
-            {answerText: 'Dorsolateral Prefrontal Cortex', isCorrect: false},
-            {answerText: 'Left Hemisphere', isCorrect: true}
+            {answerText: 'Yes', isCorrect: false},
+            {answerText: 'No', isCorrect: true}
         ]
     },
     {
-        question: "The fact that language involves functions both typical of the left and right hemispheres while lateralization develops gradually with age has led to which hypothesis?",
+        question: "What part of our body benefits from learning a language?",
         answers: [
-            {answerText: 'The Critical Period Hypothesis', isCorrect: true},
-            {answerText: 'The Critical Mass Hypothesis', isCorrect: false},
-            {answerText: 'The Compound Bilingual Hypothesis', isCorrect: false},
-            {answerText: 'The Early Laterization Hypothesis', isCorrect: false}
+            {answerText: 'The brain', isCorrect: true},
+            {answerText: 'The nervous system', isCorrect: false},
         ]
     },
     {
-        question: "A trained bilingual brain can also help delay the onset of diseases, like Alzheimer's and dementia",
+        question: "Who receive the main benefit from learning a new language?",
+        answers: [
+            {answerText: 'The cerebral cortex', isCorrect: false},
+            {answerText: 'The executive functions', isCorrect: true},
+            {answerText: 'The brain', isCorrect: false}
+        ]
+    },
+    {
+        question: "Which are the executive functions?",
+        answers: [
+            {answerText: 'Attention, problem solving, planning', isCorrect: true},
+            {answerText: 'Alzheimer\'s and Dementia', isCorrect: false},
+            {answerText: 'Brain muscles', isCorrect: false}
+        ]
+    },
+    {
+        question: "\"The research has found that all sorts of cognitively challenging activities improve executive functions.\". It's true?",
+        answers: [
+            {answerText: 'Yes', isCorrect: true},
+            {answerText: 'No', isCorrect: false}
+        ]
+    },
+    {
+        question: "Playing video games can make you better at assessing risks and placing bets",
         answers: [
             {answerText: 'True', isCorrect: true},
             {answerText: 'False', isCorrect: false}
         ]
     },
     {
-        question: "Being bilingual will make me smarter?",
+        question: "Switching between vocabularies can help us to be a better multitasker",
         answers: [
-            {answerText: 'Yes', isCorrect: false},
-            {answerText: 'No', isCorrect: true},
-            {answerText: 'Not always the case', isCorrect: false}
+            {answerText: 'True', isCorrect: true},
+            {answerText: 'False', isCorrect: false}
+        ]
+    },
+    {
+        question: "Do other complex cognitive activities seem to prevent or delay dementia?",
+        answers: [
+            {answerText: 'Yes', isCorrect: true},
+            {answerText: 'No', isCorrect: false}
+        ]
+    },
+    {
+        question: "learning a language does make you smarter.",
+        answers: [
+            {answerText: 'No', isCorrect: false},
+            {answerText: 'Maybe not', isCorrect: false},
+            {answerText: 'Probably', isCorrect: true},
+            {answerText: 'Yes', isCorrect: false}
         ]
     }
 ]
